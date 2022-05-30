cee2122c63bce83117f4474a3c8d166ed4cec7df
唐鼎 <568783010@qq.com>
2022-05-30 22:01:01 +0800

    adjust cdn url

--- themes/hexo-theme-icarus-master/includes/helpers/cdn.js
+++ themes/hexo-theme-icarus-master/includes/helpers/cdn.js
@@ -8,7 +8,7 @@
  */
 const cdn_providers = {
     cdnjs: 'https://cdnjs.cloudflare.com/ajax/libs/${ package }/${ version }/${ filename }',
-    jsdelivr: 'https://cdn.jsdelivr.net/npm/${ package }@${ version }/${ filename }',
+    jsdelivr: 'https://lib.baomitu.com/${ package }${ version }/${ filename }',
     unpkg: 'https://unpkg.com/${ package }@${ version }/${ filename }'
 };
 
